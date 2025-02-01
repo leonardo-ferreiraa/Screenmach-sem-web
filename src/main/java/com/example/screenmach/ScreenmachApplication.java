653eaecb8f80375c88d2b306c1b220d6f705f796
leonardo-ferreiraa <leonardofsv509@gmail.com>
2025-02-01 12:48:53 -0300

    Inicio do projeto Spring

---
+++ src/main/java/com/example/screenmach/ScreenmachApplication.java
@@ -0,0 +1,26 @@
+package com.example.screenmach;
+
+import com.example.screenmach.model.DadosSerie;
+import com.example.screenmach.service.ConsumoApi;
+import com.example.screenmach.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmachApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmachApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/test/java/com/example/screenmach/ScreenmachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.screenmach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
